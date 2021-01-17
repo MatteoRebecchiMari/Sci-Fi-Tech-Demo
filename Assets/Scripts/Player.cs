bb636c5e3cf6e4f62f39c4ca0f026824b9acb210
Matteo Rebecchi Mari <dev.matteo.rebecchimari@gmail.com>
2021-01-17 10:30:59 +0100

    Added Player Script, using Character Controller (handle player movements)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    CharacterController _controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        if(_controller == null)
+        {
+            Debug.LogError("CharacterController not found!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovements();
+    }
+
+    [SerializeField]
+    float _speed = 3.5f;
+
+    [SerializeField]
+    float _gravity = 9.81f;
+
+    /// <summary>
+    /// Handle player movementes
+    /// </summary>
+    void HandleMovements()
+    {
+        // Handle User Input
+        // NOTE: GetAxis --> See Input Manager in Unity Editor
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        // Convert the input into motion DIRECTION
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+
+        // Handle Gravity
+        if (_controller.isGrounded == false)
+        {
+            direction.y -= _gravity;
+        }
+
+
+        // Move the player
+        _controller.Move(direction * _speed * Time.deltaTime);
+
+    }
+
+}
